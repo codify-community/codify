1f19fa4536c756c98bff0b24e7e8a80af5eb484d
Bielgomes <bielgomesdasilva@hotmail.com>
2025-05-08 02:13:23 -0300

    build: add react helmet async lib to better SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Helmet, HelmetProvider } from "react-helmet-async"
+
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <h1>Hello World</h1>
-    </div>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | codify" />
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <h1>Hello World</h1>
+      </div>
+    </HelmetProvider>
   )
 }
 
